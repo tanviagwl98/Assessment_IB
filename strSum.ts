b7b7d246a84cc565b75382e1c61e98befa189c94
Tanvi Agarwal <45534001+tanviagwl98@users.noreply.github.com>
2024-06-25 13:12:25 +0530

    Update strSum.ts

--- strSum.ts
+++ strSum.ts
@@ -1,23 +1,38 @@
 function strFunc(strA: string): number {
   let delimiter: string = ',';
   let num: string = strA;
+
+  // Check if delimiter exists and replace that to find numbers as a string in provided string.
   
   if (strA.startsWith('//')) {
     const delimiterEndInd: number = strA.indexOf('\n');
     delimiter = strA.substring(2, delimiterEndInd);
     num = strA.substring(delimiterEndInd + 1);
   }  
+
+  // Replacing \n in case it is present
   let x: string[] = num.replace(/\n/g, delimiter).split(delimiter);
   let sum: number = 0;
-  
+  let negativeNum : number []= [] 
   for (let i = 0; i < x.length; i++) {
+    
+  // Condition to check if negative number exists 
+    if(Number(x[i]) < 0){
+  	negativeNum.push(Number(x[i]))
+  }
     sum += Number(x[i]);
+  }
+  
+  // Throw error if negative numbers are there.
+  if (negativeNum.length>0){
+  	throw new Error(`Negative numbers not allowed: ${negativeNum.join			(', ')}`);
   }
   
   return sum;
 }
 
 // Test cases
-console.log(strFunc('1,2,3')); 
 console.log(strFunc('1\n2,3')); 
+console.log(strFunc('//;\n1;-2;-3'));
+console.log(strFunc('//;\n1;2'));
 console.log(strFunc('//;\n1;2;3'));
