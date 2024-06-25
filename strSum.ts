1b8feecac2fffb156e14635b3e09407e742610b7
Tanvi Agarwal <45534001+tanviagwl98@users.noreply.github.com>
2024-06-25 11:01:52 +0530

    Create strSum.ts

---
+++ strSum.ts
@@ -0,0 +1,22 @@
+function strFunc(strA: string): number {
+  let delimiter: string = ',';
+  let num: string = strA;
+  
+  if (strA.startsWith('//')) {
+    const delimiterEndInd: number = strA.indexOf('\n');
+    delimiter = strA.substring(2, delimiterEndInd);
+    num = strA.substring(delimiterEndInd + 1);
+  }  
+  let x: string[] = num.replace(/\n/g, delimiter).split(delimiter);
+  let sum: number = 0;
+  
+  for (let i = 0; i < x.length; i++) {
+    sum += Number(x[i]);
+  }
+  
+  return sum;
+}
+
+// Test cases
+console.log(strFunc('1\n2,3')); 
+console.log(strFunc('//;\n1;2'));
